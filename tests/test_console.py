33ef5eb545816760eb501b46b1c80585274c9aae
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-11 16:20:59 +0100

    added tests for tasks 11,12,13,14,15,16

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from unittest.mock import patch
 import json
 import re
+import sys
 from uuid import UUID, uuid4
 from datetime import datetime
 from time import sleep
@@ -115,8 +116,12 @@ class TestHBNBCommand(TestCase):
             self.assertTrue(('[User] (' + user_id + ')') in buf.getvalue())
             clio(buf)
 
-            # test [ update ] command in the last user created with comma seperated args
-            console.onecmd('update User ' + user_id + ' first_name updatedName')
+            # test [ update ] command in the last user created with
+            # comma seperated args
+            console.onecmd(
+                'update User ' + user_id +
+                ' first_name updatedName'
+                )
             console.onecmd('show User ' + user_id)
             self.assertTrue("'first_name': 'updatedName'" in buf.getvalue())
             clio(buf)
@@ -126,4 +131,105 @@ class TestHBNBCommand(TestCase):
             self.assertEqual(buf.getvalue(), "")
             console.onecmd('show User ' + user_id)
             self.assertEqual(buf.getvalue(), "** no instance found **\n")
+            clio(buf)
+
+    def test_11(self):
+        '''tests for task 11 about dot commands syntax for the all command'''
+        console = HBNBCommand()
+        buf = StringIO()
+
+        with patch('sys.stdout', new=buf) as f:
+            console.onecmd('create State')
+            state_id = buf.getvalue().strip()
+            clio(buf)
+            line = console.precmd('State.all()')
+            console.onecmd(line)
+            self.assertTrue(state_id in buf.getvalue())
+            clio(buf)
+
+    def test_12(self):
+        '''tests for task 12 about dot commands syntax for the count command'''
+        console = HBNBCommand()
+        buf = StringIO()
+
+        with patch('sys.stdout', new=buf) as f:
+            console.onecmd('create State')
+            clio(buf)
+            line = console.precmd('State.count()')
+            console.onecmd(line)
+            self.assertTrue(0 <= int(buf.getvalue()))
+            clio(buf)
+
+    def test_13(self):
+        '''tests for task 13 about dot commands syntax for the show command'''
+        console = HBNBCommand()
+        buf = StringIO()
+
+        with patch('sys.stdout', new=buf) as f:
+            console.onecmd('create State')
+            state_id = buf.getvalue().strip()
+            clio(buf)
+            line = console.precmd('State.show(' + state_id + ')')
+            console.onecmd(line)
+            self.assertTrue(state_id in buf.getvalue())
+            clio(buf)
+
+    def test_14(self):
+        ''' tests for task 14 about dot commands syntax for
+            the destroy command
+        '''
+        console = HBNBCommand()
+        buf = StringIO()
+
+        with patch('sys.stdout', new=buf) as f:
+            console.onecmd('create Review')
+            review_id = buf.getvalue().strip()
+            clio(buf)
+            line = console.precmd('Review.destroy(' + review_id + ')')
+            console.onecmd(line)
+            clio(buf)
+            console.onecmd('show Review ' + review_id)
+            self.assertEqual(buf.getvalue(), "** no instance found **\n")
+            clio(buf)
+
+    def test_15(self):
+        '''tests for task 15 about dot commands syntax for the update command
+            with quoted strings
+        '''
+        console = HBNBCommand()
+        buf = StringIO()
+
+        with patch('sys.stdout', new=buf) as f:
+            console.onecmd('create Review')
+            review_id = buf.getvalue().strip()
+            clio(buf)
+            line = console.precmd(
+                'Review.update(' + review_id +
+                ', text, "this room is good")'
+                )
+            console.onecmd(line)
+            console.onecmd('show Review ' + review_id)
+            self.assertTrue("'text': 'this room is good'" in buf.getvalue())
+            clio(buf)
+
+    def test_16(self):
+        '''tests for task 16 about dot commands syntax for the update command
+            but using dictionarry arguments and quoted strings
+        '''
+        console = HBNBCommand()
+        buf = StringIO()
+
+        with patch('sys.stdout', new=buf) as f:
+            console.onecmd('create Review')
+            review_id = buf.getvalue().strip()
+            clio(buf)
+            line = console.precmd(
+                'Review.update(' + review_id +
+                ", {'text': 'worth it', 'user name': 'not me'})"
+                )
+            console.onecmd(line)
+            console.onecmd('show Review ' + review_id)
+            self.assertTrue(
+                "'text': 'worth it', 'user name': 'not me'" in buf.getvalue()
+                )
             clio(buf)
