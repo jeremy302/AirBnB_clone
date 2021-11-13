70c4b42eb5ea4921132e97af6426e97e12618204
jeremy302 <olajeremy123@gmail.com>
2021-11-13 12:35:25 +0100

    simplified syntax

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,15 +20,19 @@ class FileStorage:
 
     def new(self, obj):
         ''' registers a new object '''
-        self.__objects['{}.{}'.format(
-            obj.__class__.__name__, obj.id)] = obj
+        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
         ''' saves all objects to a file '''
         with open(self.__file_path, 'w') as file:
             r_objs = self.__objects
-            objs = {k: r_objs[k] if type(r_objs[k]) is dict
-                    else r_objs[k].to_dict() for k in r_objs}
+            objs = {}
+            for k in r_objs:
+                v = r_objs[k]
+                objs[k] = v.to_dict()
+            # objs = {k: r_objs[k] if type(r_objs[k]) is dict
+            #         else r_objs[k].to_dict() for k in r_objs}
             json.dump(objs, file)
 
     def reload(self):
@@ -37,5 +41,10 @@ class FileStorage:
         if isfile(self.__file_path):
             with open(self.__file_path, 'r') as file:
                 js_objs = json.load(file)
-                self.__objects = {k: clss[v['__class__']](**v)
-                                  for k, v in js_objs.items()}
+                self.__objects.clear()
+                # self.__objects = {}
+                for k, v in js_objs.items():
+                    cls = clss[v['__class__']]
+                    self.__objects[k] = cls(**v)
+                # self.__objects = {k: clss[v['__class__']](**v)
+                #                   for k, v in js_objs.items()}
