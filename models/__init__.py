2319a69c38228ed83f4c205236d26ae78044db37
jeremy302 <olajeremy123@gmail.com>
2021-11-13 11:13:07 +0100

    fixed circular import

--- models/__init__.py
+++ models/__init__.py
@@ -2,18 +2,23 @@
 ''' creates a static FileStorage instance '''
 from .engine.file_storage import FileStorage
 
-
-from .base_model import BaseModel
-from .amenity import Amenity
-from .city import City
-from .place import Place
-from .review import Review
-from .state import State
-from .user import User
-
-
 storage = FileStorage()
+models = {}
 
-models = {c.__name__: c
-          for c in [BaseModel, Amenity, City, Place, Review, State, User]}
+
+def import_models():
+    global models
+    from .base_model import BaseModel
+    from .amenity import Amenity
+    from .city import City
+    from .place import Place
+    from .review import Review
+    from .state import State
+    from .user import User
+
+    models = {c.__name__: c
+              for c in [BaseModel, Amenity, City, Place, Review, State, User]}
+
+
+import_models()
 storage.reload()
