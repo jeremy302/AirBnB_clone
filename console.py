88fcc9f9391da6b3c1aec2726c23993f41b434d2
jeremy302 <olajeremy123@gmail.com>
2021-11-13 14:54:28 +0100

    reverted prompt

--- console.py
+++ console.py
@@ -37,7 +37,7 @@ class HBNBCommand(cmd.Cmd):
         """Initializes command interpreter"""
         super().__init__()
         # determines prompt for interactive/non-interactive modes
-        self.prompt = '(hbnb) ' if sys.stdin.isatty() else '(hbnb)\n'
+        self.prompt = '(hbnb) ' if sys.stdin.isatty() else ''
 
     def preloop(self):
         """Prints the prompt when isatty is false"""
