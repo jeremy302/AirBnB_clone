d6904a2ee206c5248e8b86ef08e0e216ea3e2ecd
jeremy302 <olajeremy123@gmail.com>
2021-11-13 13:04:54 +0100

    testing pep8 fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,6 +23,10 @@ class FileStorage:
         key = '{}.{}'.format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
+    def reload(self):
+        ''' loads data from file '''
+        return 4
+
     def save(self):
         ''' saves all objects to a file '''
         with open(self.__file_path, 'w') as file:
@@ -32,7 +36,3 @@ class FileStorage:
                 v = r_objs[k]
                 objs[k] = v.to_dict()
             json.dump(objs, file)
-
-    def reload(self):
-        ''' loads data from file '''
-        return 4
