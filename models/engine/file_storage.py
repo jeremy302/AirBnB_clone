73e378d97ce0bce424ea881cb1cc2e015b554292
jeremy302 <olajeremy123@gmail.com>
2021-11-14 18:38:40 +0100

    removed complex syntax

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,21 +16,15 @@ class FileStorage:
 
     def new(self, obj):
         ''' registers a new object '''
-        self.__objects['{}.{}'.format(
-            obj.__class__.__name__, obj.id)] = obj
+        pass
 
     def save(self):
         ''' saves all objects to a file '''
         with open(self.__file_path, 'w') as file:
-            r_objs = self.__objects
-            objs = {k: r_objs[k].to_dict() for k in r_objs}
-            json.dump(objs, file)
+            pass
 
     def reload(self):
         ''' load objects from a file '''
         clss = models.models
         if isfile(self.__file_path):
-            with open(self.__file_path, 'r') as file:
-                js_objs = json.load(file)
-                self.__objects = {k: clss[v['__class__']](**v)
-                                  for k, v in js_objs.items()}
+            pass
