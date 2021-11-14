be7e1ae06700c53eac63af737c6c25cee5f717c0
jeremy302 <olajeremy123@gmail.com>
2021-11-14 21:31:20 +0100

    restore

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 ''' module for FileStorage class '''
 import json
 from os.path import isfile
+import models
 
 
 class FileStorage:
@@ -11,17 +12,25 @@ class FileStorage:
 
     def all(self):
         ''' gets all objects '''
-        return self.__objects
+        return self.__class__.__objects
 
     def new(self, obj):
         ''' registers a new object '''
-        pass
+        self.__class__.__objects['{}.{}'.format(
+            obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         ''' saves all objects to a file '''
-        with open(self.__file_path, 'w') as file:
-            pass
+        with open(self.__class__.__file_path, 'w') as file:
+            r_objs = self.__class__.__objects
+            objs = {k: r_objs[k].to_dict() for k in r_objs}
+            json.dump(objs, file)
 
     def reload(self):
         ''' load objects from a file '''
-        pass
+        clss = models.models
+        if isfile(self.__class__.__file_path):
+            with open(self.__class__.__file_path, 'r') as file:
+                js_objs = json.load(file)
+                type(self).__objects = {k: clss[v['__class__']](**v)
+                                        for k, v in js_objs.items()}
