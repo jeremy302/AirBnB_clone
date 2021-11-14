04dbcceeeeec55d0d94b21c18b0217c286c5e5b9
jeremy302 <olajeremy123@gmail.com>
2021-11-14 21:18:55 +0100

    different date instances

--- models/base_model.py
+++ models/base_model.py
@@ -2,13 +2,13 @@
 ''' module for BaseModel class '''
 from uuid import uuid4
 from datetime import datetime
-from . import storage
 
 
 class BaseModel:
     ''' class of the base model of higher-level data models '''
     def __init__(self, *arg, **kwargs):
         ''' BaseModel constructor '''
+        from . import storage
         if kwargs:
             for k in kwargs:
                 if k in ['created_at', 'updated_at']:
@@ -18,11 +18,12 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at.replace()
+            self.updated_at = datetime.utcnow()
             storage.new(self)
 
     def save(self):
         ''' saves a model '''
+        from . import storage
         self.updated_at = datetime.utcnow()
         storage.save()
 
