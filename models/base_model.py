4c5a7a7f599996a67909b425d962b3eb07db5e92
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-15 10:16:15 +0100

    update

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel:
 
     def save(self):
         ''' saves a model '''
-        from . import storage
+        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
