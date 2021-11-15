3ef372c27200c097fae857119de913a0d266067e
jeremy302 <olajeremy123@gmail.com>
2021-11-15 03:50:10 +0100

    added blank line

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@
 ''' creates a static FileStorage instance '''
 from .engine.file_storage import FileStorage
 
+
 storage = FileStorage()
 # storage._FileStorage__file_path = 'data.json'
 # storage._FileStorage__objects = {}
