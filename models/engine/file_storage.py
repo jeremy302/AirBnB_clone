f17f1be283b7638e7d66f288c031727f9e8eae7b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-11-15 10:09:13 +0100

    undo last commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -24,7 +24,7 @@ class FileStorage:
         with open(self.__file_path, 'w') as file:
             r_objs = self.__objects
             objs = {k: r_objs[k].to_dict() for k in r_objs}
-            json.dumps(objs, file)
+            json.dump(objs, file)
 
     def reload(self):
         ''' load objects from a file '''
